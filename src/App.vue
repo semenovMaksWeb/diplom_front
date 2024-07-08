b8640961aff8eb7ab303a3072a55a9798d66edd6
semenov <semenov>
2024-07-08 14:54:59 +0400

    component nav

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <header class="header">
+    <div class="container container_header">
+      <NavComponent />
+      <router-link class="link nav__link" to="/">Выход</router-link>
+    </div>
+  </header>
+  <main class="main">
+    <div class="container">
+      <router-view />
+    </div>
+  </main>
 </template>
 
+<script setup lang="ts">
+import NavComponent from "@/components/ui/nav/Nav.vue";
+</script>
+
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import "src/assets/main.less";
 </style>
---
+++ src/components/ui/nav/Nav.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="nav">
+    <router-link class="link nav__link" to="/foo">Главная</router-link>
+    <router-link class="link nav__link" to="/client">Клиенты</router-link>
+    <router-link class="link nav__link" to="/developer">
+      Разработчики
+    </router-link>
+    <router-link class="link nav__link" to="/task">Задачи</router-link>
+    <router-link class="link nav__link" to="/contract">Договора</router-link>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "NavComponent",
+});
+</script>
+
+<style lang="less">
+@import "Nav";
+</style>
