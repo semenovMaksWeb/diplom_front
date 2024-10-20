f57b15c7cba526d781a806315aff659fd2af977d
semenov <semenov>
2024-10-20 11:07:53 +0400

    table and авторизация

---
+++ src/components/Client/TableClient/TableClient.vue
@@ -0,0 +1,18 @@
+<template>
+    <VueTableLite v-bind="table" />
+</template>
+
+<script lang="ts">
+
+import VueTableLite from "vue3-table-lite/ts";
+import { FormTableClient } from "./TableClient";
+ 
+export default {
+    setup() {
+        return {
+            ...FormTableClient()
+        }
+    },
+    components: { VueTableLite }
+}
+</script>
