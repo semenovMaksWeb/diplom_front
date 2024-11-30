763be0ed99f0467bdd73d2cbb130a24c67eec4c4
semenov <semenov>
2024-11-30 13:49:20 +0400

    add todo

--- src/components/Task/TaskForm/TaskForm.vue
+++ src/components/Task/TaskForm/TaskForm.vue
@@ -1,15 +1,13 @@
 <template>
-    <vue-final-modal v-model="show" name="taskForm">
-        <template #title>$vfm.show</template>
-        <template #default="{ params }">
-            Hi
-        </template>
+    <vue-final-modal v-model="show" classes="modal-container" content-class="modal-content">
+        Hi
     </vue-final-modal>
 </template>
 
 <script>
 import store from '@/store';
 import { VueFinalModal, ModalsContainer } from 'vue-final-modal'
+import 'vue-final-modal'
 
 export default {
     computed: {
@@ -23,3 +21,33 @@ export default {
     }
 }
 </script>
+
+<style>
+::v-deep .modal-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+::v-deep .modal-content {
+    display: flex;
+    flex-direction: column;
+    margin: 0 1rem;
+    padding: 1rem;
+    border: 1px solid #e2e8f0;
+    border-radius: 0.25rem;
+    background: #fff;
+}
+
+.modal__title {
+    font-size: 1.5rem;
+    font-weight: 700;
+}
+</style>
+
+<style>
+.dark-mode div::v-deep .modal-content {
+    border-color: #2d3748;
+    background-color: #1a202c;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "vue3-toastify/dist/index.css";
+import * as a from "vue-final-modal";
 
 createApp(App).use(store).use(router).mount("#app");
